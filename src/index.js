b2111ea0fcdf51e0d671f2ce485fe494f035fb6a
vishalrajole <vishalrajole1991@gmail.com>
2021-05-16 01:14:18 +0200

    path join to fix heroku

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ async function startApolloServer() {
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
   }
 

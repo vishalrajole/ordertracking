84f22110a2e0e7e79dde9249bf3f8d3163109ee4
vishalrajole <vishalrajole1991@gmail.com>
2021-05-16 00:52:48 +0200

    node js modules not exporting __dirname in path module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path";
+import { path, dirname } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
 // Bring in GraphQL-Express middleware
